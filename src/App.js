edabccace3f323418b26b53e998328be2e9c1e54
pahadi-guy <vikasuv11@gmail.com>
2022-02-03 22:33:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import FetchApi from "./fetch";
 
 import Home from "./home";
-import Login from "./login";
 
 function App() {
   return (
     <>
       {/* <FetchApi /> */}
-      {/* <Login /> */}
+
       <Home />
     </>
   );
